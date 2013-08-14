ef60351c52964cf91dc06ba2881425ecbf4ea42d
Anant Shrivastava <anant.shrivastava@gmail.com>
2013-08-14 12:47:41 +0530

    XMLRPC related functions, disable and remove header

--- theme_functions/xmlrpc.functions.php
+++ theme_functions/xmlrpc.functions.php
@@ -1,3 +1,4 @@
+#USECASE : Block would useful to remove X-PingBack Header from http response
 //remove xpingback header
 function remove_x_pingback($headers) {
     unset($headers['X-Pingback']);
@@ -5,6 +6,8 @@ function remove_x_pingback($headers) {
 }
 add_filter('wp_headers', 'remove_x_pingback');
 
+
+#USECASE : Disable XMLRPC Class compeletely
 /*Disable complete xmlrpc class. */
 add_filter( 'wp_xmlrpc_server_class', '__return_false' );
 add_filter('xmlrpc_enabled', '__return_false');
